8bbd61f2b76a398fc6259564d878a7c9f184f5f7
Boclair Temgoua <temgoua2000@gmail.com>
2022-10-11 16:45:51 +0200

    fix(general): fix module

--- server-api/src/app.module.ts
+++ server-api/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
+import {
+  AppDataSource,
+  AppSeedDataSource,
+} from './infrastructure/databases/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(AppDataSource.options),
+    TypeOrmModule.forRoot(AppSeedDataSource.options),
+    ScheduleModule.forRoot(),
+  ],
 })
 export class AppModule {}
--- server-api/src/main.ts
+++ server-api/src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configurations } from './infrastructure/configurations';
+import helmet from 'helmet';
+import * as path from 'path';
 
 async function bootstrap() {
+  const port = configurations.port;
+  const version = configurations.api.version;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix(`/api/${version}`);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.enableCors();
+  app.use(helmet());
+  await app.listen(port, () => {
+    console.log(`=============================================`);
+    console.log(`*** 🚀 Link  http://localhost:${port}/api/${version} ***`);
+    console.log(`=============================================`);
+  });
 }
 bootstrap();
